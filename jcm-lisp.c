4154ff9363c0150eae221055ccabadaa5a19d0b3
Jonathon McKitrick <jmckitrick@gmail.com>
2017-10-17 21:35:35 -0400

    Working basic string

--- jcm-lisp.c
+++ jcm-lisp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum {
     NUMBER
@@ -24,13 +25,36 @@ struct object {
 
 typedef struct object object;
 
+object *new_object() {
+    object *obj = malloc(sizeof(object));
+    return obj;
+}
+
+object *make_string(char *str) {
+    object *obj = new_object();
+    obj->data.str.text = malloc(4);
+    strncpy(obj->data.str.text, str, 4);
+    return obj;
+}
+
 object *read(FILE *in) {
+    char str[] = "yes";
+    str[3] = '\0';
+    return make_string(str);
 }
 
 object *eval(object *exp, object *env) {
+    return exp;
 }
 
 void print(object *exp, object *env) {
+    char *str = exp->data.str.text;
+    putchar('"');
+    while (*str != '\0') {
+        putc(*str, stdout);
+        str++;
+    }
+    putchar('"');
 }
 
 int main (int argc, char* argv[])
@@ -38,11 +62,11 @@ int main (int argc, char* argv[])
     printf("Welcome to JCM-LISP. "
            "Use ctrl-c to exit.\n");
 
-    while (1) {
+//    while (1) {
         printf("> ");
-        print(eval(read(stdin)));
+        print(eval(read(stdin), NULL), NULL);
         printf("\n");
-    }
+//    }
 
     return 0;
 }
