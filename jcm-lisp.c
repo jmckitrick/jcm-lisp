646f36cc4e1bbba0e0b2fe07429aba8b6d2df243
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2017-10-19 07:58:12 -0400

    Working string read

--- jcm-lisp.c
+++ jcm-lisp.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+#define MAX_BUFFER_SIZE 100
 
 typedef enum {
-    NUMBER
+    NUMBER,
+    STRING
 } obj_type;
 
 struct number {
@@ -32,29 +36,74 @@ object *new_object() {
 
 object *make_string(char *str) {
     object *obj = new_object();
-    obj->data.str.text = malloc(4);
-    strncpy(obj->data.str.text, str, 4);
+    obj->type = STRING;
+    obj->data.str.text = malloc(strlen(str));
+    strncpy(obj->data.str.text, str, strlen(str));
+    printf("got here 2\n");
     return obj;
 }
 
-object *read(FILE *in) {
-    char str[] = "yes";
-    str[3] = '\0';
-    return make_string(str);
-}
-
-object *eval(object *exp, object *env) {
-    return exp;
-}
-
-void print(object *exp, object *env) {
-    char *str = exp->data.str.text;
-    putchar('"');
-    while (*str != '\0') {
-        putc(*str, stdout);
-        str++;
+int is_whitespace(char c) {
+    if (isspace(c)) {
+        return 1;
+    } else {
+        return 0;
     }
-    putchar('"');
+}
+
+void skip_whitespace(FILE *in) {
+    char c;
+
+    while ((c = getc(in)) != '\n') {
+        if (is_whitespace(c))
+            continue;
+
+        break;
+    }
+
+    ungetc(c, in);
+}
+
+object *read(FILE *in) {
+    char buffer[MAX_BUFFER_SIZE];
+    char c;
+    int i = 0;
+
+    skip_whitespace(in);
+
+    while ((c = getc(in)) != '\n' && i < MAX_BUFFER_SIZE - 1) {
+        buffer[i++] = c;
+    }
+
+    printf("got here 1 %d\n", i);
+    buffer[i] = '\0';
+    return make_string(buffer);
+}
+
+object *eval(object *obj, object *env) {
+    printf("got here 3xx\n");
+    return obj;
+}
+
+void print(object *obj, object *env) {
+    printf("got here 3\n");
+    char *str = obj->data.str.text;
+    int len = strlen(str);
+    int i = 0;
+    printf("got here 4\n");
+
+    switch (obj->type) {
+        case STRING:
+            putchar('"');
+            while (i < len) {
+                putc(*str++, stdout);
+                i++;
+            }
+            putchar('"');
+            break;
+        default:
+            break;
+    }
 }
 
 int main (int argc, char* argv[])
