11236c4be22468dfd8c49d989738a4fd5a04b40a
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2017-10-19 16:30:08 -0400

    Improve readers, add 2 element list

--- jcm-lisp.c
+++ jcm-lisp.c
@@ -2,28 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #define MAX_BUFFER_SIZE 100
 
 typedef enum {
-    FIXNUM,
-    STRING
+    FIXNUM = 1,
+    STRING,
+    CELL,
+    NIL
 } obj_type;
 
-struct fixnum {
+struct Fixnum {
     int value;
 };
 
-struct string {
+struct String {
     char *text;
+};
+
+struct object;
+
+struct cell {
+    struct object *head;
+    struct object *tail;
 };
 
 struct object {
     obj_type type;
 
     union {
-        struct fixnum num;
-        struct string str;
+        struct Fixnum num;
+        struct String str;
+        struct cell cell;
     } data;
 };
 
@@ -31,6 +42,8 @@ typedef struct object object;
 
 object *new_object() {
     object *obj = malloc(sizeof(object));
+    obj->data.cell.head = NULL;
+    obj->data.cell.tail = NULL;
     return obj;
 }
 
@@ -39,7 +52,7 @@ object *make_string(char *str) {
     obj->type = STRING;
     obj->data.str.text = malloc(strlen(str));
     strncpy(obj->data.str.text, str, strlen(str));
-    printf("got here 2\n");
+    printf("make string\n");
     return obj;
 }
 
@@ -48,6 +61,20 @@ object *make_fixnum(int n) {
     obj->type = FIXNUM;
     obj->data.num.value = n;//malloc(sizeof(int));
     printf("make fixnum\n");
+    return obj;
+}
+
+object *make_cell() {
+    object *obj = new_object();
+    obj->type = CELL;
+    printf("make cell\n");
+    return obj;
+}
+
+object *make_nil() {
+    object *obj = new_object();
+    obj->type = NIL;
+    printf("make NIL\n");
     return obj;
 }
 
@@ -77,12 +104,12 @@ object *read_string(FILE *in) {
     char c;
     int i = 0;
 
-    printf("read string");
+    printf("read string\n");
     while ((c = getc(in)) != '\n' && i < MAX_BUFFER_SIZE - 1) {
         buffer[i++] = c;
     }
 
-    printf("got here 1 %d\n", i);
+    //printf("got here 1 %d\n", i);
     buffer[i] = '\0';
     return make_string(buffer);
 }
@@ -91,7 +118,7 @@ object *read_number(FILE *in) {
     int number = 0;
     char c;
 
-    printf("read number");
+    printf("read number\n");
     while (isdigit(c = getc(in))) {
         number *= 10;
         number += (int)c - 48;
@@ -102,36 +129,73 @@ object *read_number(FILE *in) {
     return make_fixnum(number);
 }
 
-object *read(FILE *in) {
+object *read2(FILE *in);
+void print(object *, object *);
+
+object *read_list(FILE *in) {
+    char c = getc(in);          /* Should be '(' */
+
+    printf("read list\n");
+    object *head = read2(in);
+    //printf("Head\n");
+    //print(head, NULL);
+
+    //object *obj = make_cell();
+    obj->data.cell.head = head;
+
+    object *tail = NULL;
+
+    do {
+        printf("Read tail\n");
+        //ungetc(c, in);
+        tail = read2(in);
+        obj->data.cell.tail = tail;
+    } while ((c = getc(in)) != ')');
+
+    //printf("Tail\n");
+    //print(tail, NULL);
+    //printf("populating cell\n");
+    
+    return obj;
+}
+
+object *read2(FILE *in) {
     char c;
-    object *obj;
+    object *obj = NULL;
 
     skip_whitespace(in);
 
     c = getc(in);
     ungetc(c, in);
 
-    printf("check char %c\n", c);
+    printf("Read found %c\n", c);
+    
+    //printf("check char %c\n", c);
     if (isdigit((int)c)) {
         obj = read_number(in);
-    } else {
+    } else if (isalpha((int)c)) {
         obj = read_string(in);
+    } else if (c == '(') {
+        obj = read_list(in);
+    } else if (c == ')') {
+        obj = make_nil();
     }
 
+    //sleep(1);
     return obj;
 }
 
 object *eval(object *obj, object *env) {
-    printf("got here 3xx\n");
+    printf("eval\n");
     return obj;
 }
 
 void print_string(object *obj) {
-    printf("got here 5\n");
+    //printf("print stirng \n");
     char *str = obj->data.str.text;
     int len = strlen(str);
     int i = 0;
-    printf("got here 6\n");
+    //printf("got here 6\n");
 
     putchar('"');
     while (i < len) {
@@ -142,12 +206,31 @@ void print_string(object *obj) {
 }
 
 void print_fixnum(object *obj) {
-    printf("got here 4\n");
+    //printf("got here 4\n");
     printf("%d", obj->data.num.value);
 }
 
+void print(object *obj, object *env);
+
+void print_cell(object *obj, object *env) {
+    printf("Cell:\n");
+    printf("(");
+    if (obj->data.cell.head->type != NIL) {
+        //printf("Head:\n");
+        print(obj->data.cell.head, env);
+    }
+    if (obj->data.cell.tail->type != NIL) {
+        //printf("Tail:\n");
+        printf(" ");
+        print(obj->data.cell.tail, env);
+    }
+    printf(") ");
+}
+
 void print(object *obj, object *env) {
-    printf("got here 3\n");
+    //printf("print\n");
+    //printf("Obj %p\n", obj);
+    //printf("Type %d\n", obj->type);
 
     switch (obj->type) {
         case STRING:
@@ -156,7 +239,14 @@ void print(object *obj, object *env) {
         case FIXNUM:
             print_fixnum(obj);
             break;
+        case CELL:
+            print_cell(obj, env);
+            break;
+        case NIL:
+            printf("NIL");
+            break;
         default:
+            printf("Unknown object\n");
             break;
     }
 }
@@ -168,7 +258,7 @@ int main (int argc, char* argv[])
 
 //    while (1) {
         printf("> ");
-        print(eval(read(stdin), NULL), NULL);
+        print(eval(read2(stdin), NULL), NULL);
         printf("\n");
 //    }
 
