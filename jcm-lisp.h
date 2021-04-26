5a0a6063f0084314f30a1995b17dbb7fa28aa0ed
Jonathon McKitrick <jmckitrick@gmail.com>
2021-04-26 13:40:47 -0400

    Define cdar/cddr

--- jcm-lisp.h
+++ jcm-lisp.h
@@ -93,5 +93,7 @@ Object *top_env;
 
 #define caar(obj)    car(car(obj))
 #define cadr(obj)    car(cdr(obj))
+#define cdar(obj)    cdr(car(obj))
+#define cddr(obj)    cdr(cdr(obj))
 
 Object *read_list(FILE *);
